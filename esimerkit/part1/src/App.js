10e728968e8f9621ead420eace383b1777afd70c
ThePikefish <kristo.jonsson@gmail.com>
2023-07-04 20:19:46 +0300

    osan 2 esimerkin alustus

--- esimerkit/part1/src/App.js
+++ esimerkit/part1/src/App.js
@@ -1,57 +1,14 @@
-import { useState } from 'react'
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
-
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
-  }
-
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-    setTotal(left + right)
-  }
+const App = (props) => {
+  const { notes } = props
 
   return (
     <div>
-      <div>
-        {left}
-        <Button handleClick={handleLeftClick} text="left" />
-        <Button handleClick={handleRightClick} text="right" />
-        {right}
-        <History allClicks={allClicks} />
-      </div>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
     </div>
   )
 }
