39958289e1de3b8017032e2ef6bec4c06c3672d0
ThePikefish <kristo.jonsson@gmail.com>
2023-07-16 17:19:49 +0300

    part2-2

---
+++ esimerkit/part2-2/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import Note from './components/Note'
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'uusi muistiinpano...'
+  )
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Muistiinpanot</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type='submit'>tallenna</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
