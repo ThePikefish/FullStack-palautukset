93aec5e96037d1095ac8f3261a97218a5c9df7ac
ThePikefish <kristo.jonsson@gmail.com>
2023-07-16 15:23:27 +0300

    Tehtävät 2.2-2.5

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -9,9 +9,9 @@ const Course = ( {course} ) => {
 
 const Header = ( {name} ) => {
     return (
-        <h1>
+        <h2>
             {name}
-        </h1>
+        </h2>
     )
 }
 
@@ -19,17 +19,24 @@ const Content = ( {parts} ) => {
     return (
         <div>
             {parts.map(part =>
-            <Part key={part.id} part={part} />
+                <Part key={part.id} part={part} />
             )}
+
+            <Total parts={parts} />
         </div>
     )
 }
 
 const Part = ( {part} ) => {
     return (
-        <p>
-            {part.name} {part.exercises}
-        </p>
+        <p> {part.name} {part.exercises} </p>
+    )
+}
+
+const Total = ( {parts} ) => {
+    const total = parts.reduce( (sum, part) => sum + part.exercises, 0 )
+    return (
+        <b>yhteensä {total} harjoitusta</b>
     )
 }
 
