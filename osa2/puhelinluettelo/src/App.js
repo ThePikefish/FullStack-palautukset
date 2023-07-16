012e2102bf3b393bbef4cd24ebf779f4e2316141
ThePikefish <kristo.jonsson@gmail.com>
2023-07-16 20:55:40 +0300

    Tehtävä 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          nimi: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">lisää</button>
+        </div>
+      </form>
+      <h2>Numerot</h2>
+      <ul>
+        {persons.map(person =>
+          <Person key={person.name} person={person} />
+        )}
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
